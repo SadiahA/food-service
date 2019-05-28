9c1715db83bd393b87aa1e7acf4156d776e465b1
Sadiah Ahmed <sadiah.ahmed@moneysavingexpert.com>
2019-05-28 12:57:55 +0100

    added more tests for foodController

--- src/test/java/com/food/foodservice/controller/FoodControllerTest.java
+++ src/test/java/com/food/foodservice/controller/FoodControllerTest.java
@@ -1,6 +1,8 @@
 package com.food.foodservice.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import com.food.foodservice.model.Food;
@@ -8,21 +10,16 @@ import com.food.foodservice.service.FoodService;
 import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.web.Link;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -30,14 +27,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(FoodController.class)
-public class FoodControllerTest1 {
+public class FoodControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private FoodService foodService;
-
 
     @Test
     public void canRetrieveFoodByIdWhenExists() throws Exception {
@@ -67,57 +63,69 @@ public class FoodControllerTest1 {
         // when + then
         this.mockMvc.perform(get("/food/5678"))
                 .andExpect(status().isNotFound());
-
     }
 
     @Test
     public void addingValidFoodItemAndReturnFoodIdAnd200Response() throws Exception {
         // given
-        // GSON is a java API from Google that converts java
-        // objects to their foodJson representations
-
-        Food food2 = new Food();
-        food2.setName("brocolli");
-        food2.setCategories(Arrays.asList("vegetables", "greens", "health-food"));
-        food2.setCost(20);
-        food2.setCalories(30);
-
-        // build the request object
-
         Gson gson = new Gson();
-        String foodJson = gson.toJson(food2);
-        given(foodService.addFood(food2)).willReturn("2345");
-        // String uuid keeps being null
+        String foodJson = gson.toJson(new Food());
+        given(foodService.addFood(any(Food.class))).willReturn("2345");
 
-//        String linkJson = gson.toJson(new Link("/food/" + "2345"));
+        String linkJson = gson.toJson(new Link("/food/" + "2345"));
 
         // when + then
         this.mockMvc.perform(post("/foods").contentType(MediaType.APPLICATION_JSON).content(foodJson))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk()).andExpect(content().string(linkJson));
+    }
+
+    // ****** Test for when InvalidFoodItem is posted ******
+
+
+    @Test
+    public void deletingAnExistingFoodItemAndReturns204Response() throws Exception {
+        // given
+        given(foodService.getFoodById(any(String.class))).willReturn(Optional.of(new Food()));
+
+        // when + then
+        this.mockMvc.perform(delete("/food/4567")).andExpect(status().isNoContent());
     }
 
     @Test
-    public void exampleTest() throws Exception {
-        Food food2 = new Food();
-        food2.setName("brocolli");
-        food2.setCategories(Arrays.asList("vegetables", "greens", "health-food"));
-        food2.setCost(20);
-        food2.setCalories(30);
+    public void deletingANonExistingFoodItemsReturnsA404Response() throws Exception {
+        // given
+        given(foodService.getFoodById(any(String.class))).willReturn(Optional.empty());
 
-        Gson gson = new Gson();
-        String foodJson = gson.toJson(food2);
-
-        Mockito.when(foodService.addFood(food2)).thenReturn("3455");
-
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/foods")
-                .accept(MediaType.APPLICATION_JSON).content(foodJson)
-                .contentType(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-        MockHttpServletResponse response = result.getResponse();
-
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        // when + then
+        this.mockMvc.perform(delete("/food/1234")).andExpect(status().isNotFound());
     }
+
+    @Test
+    public void canRetrieveAllFoodItemsWhenPresentAndReturns200Response() throws Exception {
+        // given
+        List<Food> foodList = new ArrayList<>();
+        given(foodService.getAllFoods()).willReturn(Optional.of(foodList));
+
+        // when + then
+        this.mockMvc.perform(get("/foods")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void doesNotRetrieveAllFoodItemsWhenNotPresentAndReturns404Response() throws Exception {
+        // given
+        given(foodService.getAllFoods()).willReturn(Optional.empty());
+
+        // when + then
+        this.mockMvc.perform(get("/foods")).andExpect(status().isNotFound());
+    }
+
+
+
+
+
+
+
+
+
 
 }
