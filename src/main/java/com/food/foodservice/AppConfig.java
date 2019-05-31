3f53b9d407c59626c837a0bbb1860513e7facbcd
Sadiah Ahmed <sadiah.ahmed@moneysavingexpert.com>
2019-05-31 15:09:04 +0100

    refector: changed name of hashMapRepo

--- src/main/java/com/food/foodservice/AppConfig.java
+++ src/main/java/com/food/foodservice/AppConfig.java
@@ -1,7 +1,7 @@
 package com.food.foodservice;
 
 import com.food.foodservice.repository.FoodRepository;
-import com.food.foodservice.repository.HashFoodRepository;
+import com.food.foodservice.repository.InMemoryFoodRepository;
 import com.food.foodservice.repository.MongoFoodRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +13,7 @@ public class AppConfig {
     @Bean
     @Profile("hash")
     public FoodRepository hashfoodRepository() {
-        return new HashFoodRepository();
+        return new InMemoryFoodRepository();
     }
 
     @Bean
