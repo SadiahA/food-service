d4496c54498350d05c4cb58a66cf0ce661039ac1
Sadiah Ahmed <sadiah.ahmed@moneysavingexpert.com>
2019-06-17 08:55:06 +0100

    Added foodView interface

--- src/main/java/com/food/foodservice/view/FoodView.java
+++ src/main/java/com/food/foodservice/view/FoodView.java
@@ -1,4 +1,26 @@
 package com.food.foodservice.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface FoodView {
+    public String getName();
+
+    public void setName(String name);
+
+    public List<String> getCategories();
+
+    public void setCategories(List<String> categories);
+
+    public double getCalories();
+
+    public void setCalories(double calories);
+
+    public double getCost();
+
+    public void setCost(double cost);
+
 }
+
+
+
