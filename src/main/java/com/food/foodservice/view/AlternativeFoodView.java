947d204126ac53964105dcd4516e95d28d5ef207
Sadiah Ahmed <sadiah.ahmed@moneysavingexpert.com>
2019-06-17 08:50:44 +0100

    Added new class

--- src/main/java/com/food/foodservice/view/AlternativeFoodView.java
+++ src/main/java/com/food/foodservice/view/AlternativeFoodView.java
@@ -4,18 +4,61 @@ import java.util.List;
 
 import com.food.foodservice.model.Food;
 
-public class FoodView {
+public class AlternativeFoodView implements FoodView {
 
-    private final String name;
-    private final  List<String> categories;
-    private final double calories;
-    private final double cost;
+    private String name;
+    private List<String> categories;
+    private double calories;
+    private double cost;
 
-    public FoodView(Food food) {
+    public AlternativeFoodView() {
+    }
+
+    public AlternativeFoodView(Food food) {
         this.name = "[" + food.getName() + "]";  //just an example, name doesnt have to match food name
-        this.categories = food.getCategories()
-
+        this.categories = food.getCategories();
+        this.calories = food.getCalories();
+        this.cost = food.getCost();
     }
 
 
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    @Override
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public double getCalories() {
+        return 0;
+    }
+
+    @Override
+    public void setCalories(double calories) {
+
+    }
+
+    @Override
+    public double getCost() {
+        return cost;
+    }
+
+    @Override
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
