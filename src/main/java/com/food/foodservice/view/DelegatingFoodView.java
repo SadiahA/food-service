4041e35f1b42b57a862a1757b088841a74be3c0c
Sadiah Ahmed <sadiah.ahmed@moneysavingexpert.com>
2019-06-17 08:49:12 +0100

    Added new class

--- src/main/java/com/food/foodservice/view/DelegatingFoodView.java
+++ src/main/java/com/food/foodservice/view/DelegatingFoodView.java
@@ -4,23 +4,49 @@ import java.util.List;
 
 import com.food.foodservice.model.Food;
 
-public class FoodView {
+public class DelegatingFoodView implements FoodView {
 
     private Food food;
-
-    public FoodView() {
+    
+    public DelegatingFoodView(Food food) {
+        food = new Food();
     }
 
     public String getName() {
-        return food.getName();
+        return food.getName() + " - " + food.getCategories() + "kcal";
+    }
+
+    @Override
+    public void setName(String name) {
+
     }
 
     public List<String> getCategories() {
         return food.getCategories();
     }
 
+    @Override
+    public void setCategories(List<String> categories) {
+
+    }
+
+    @Override
+    public double getCalories() {
+        return 0;
+    }
+
+    @Override
+    public void setCalories(double calories) {
+
+    }
+
     public double getCost() {
         return food.getCost();
+    }
+
+    @Override
+    public void setCost(double cost) {
+
     }
 
 }
