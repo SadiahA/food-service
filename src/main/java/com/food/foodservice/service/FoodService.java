8d85e28e6b34fbea7881e71b99da163f4210b773
Sadiah Ahmed <sadiah.ahmed@moneysavingexpert.com>
2019-06-26 10:53:54 +0100

    Changed parameter in addFood method to talk in a foodView item

--- src/main/java/com/food/foodservice/service/FoodService.java
+++ src/main/java/com/food/foodservice/service/FoodService.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import com.food.foodservice.model.Food;
 import com.food.foodservice.repository.FoodRepository;
+import com.food.foodservice.view.FoodView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,8 +34,15 @@ public class FoodService {
         return Optional.ofNullable(foodRepository.getFood(foodId));
     }
 
-    public String addFood(@Valid Food food) throws InvalidFoodException {
-        if (food != null) {
+    public String addFood(@Valid FoodView foodView) throws InvalidFoodException {
+        if (foodView != null) {
+            Food food = new Food();
+
+            food.setName(foodView.getName());
+            food.setCost(foodView.getCost());
+            food.setCalories(foodView.getCalories());
+            food.setCategories(foodView.getCategories());
+
             return foodRepository.addFood(food);
         } else {
             throw new InvalidFoodException();
